ded50248020b996222042aa3329b60eccca387da
HashemNAVA77 <143622962+HashemNAVA77@users.noreply.github.com>
2023-09-06 08:43:34 -0600

    Add files via upload

---
+++ Listadetareashnb.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listadetareashnb;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Listadetareashnb {
+
+    public static void main(String[] args) {
+        int lista;
+        string 
+        String lista = "listatareas: ";
+       String tarea1 =("levantarse temprano");
+       String tarea2 =("levantarse temprano");
+       
+    }
+}
